334196a74964c4037b843a3ecc88f6948546510a
jesus <jesus.ramon2192@gmail.com>
2023-04-17 11:52:58 -0600

    Aplicacion web smart light

--- dimmer/main/dimmer.c
+++ dimmer/main/dimmer.c
@@ -6,6 +6,11 @@
 #include "esp_http_server.h"
 #include "driver/ledc.h"
 
+extern const char index_start[] asm("_binary_index_html_start");  
+extern const char index_end[] asm("_binary_index_html_end");  
+extern const char chroma_start[] asm("_binary_chroma_png_start");  
+extern const char chroma_end[] asm("_binary_chroma_png_end"); 
+
 int32_t led_r = 0, led_g = 0, led_b = 0;
 
 #define LEDC_TIMER          LEDC_TIMER_0
@@ -33,7 +38,7 @@ static esp_err_t api_get_handler(httpd_req_t *req) {
         buf = malloc(buf_len);
 
         if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
-            char param[4];
+            char param[10];
             if (httpd_query_key_value(buf, "r", param, sizeof(param)) == ESP_OK) {
                 led_r = atoi(param);
                 printf("r: %ld\n", led_r);
@@ -67,10 +72,40 @@ static esp_err_t api_get_handler(httpd_req_t *req) {
     return ESP_OK;
 }
 
+static esp_err_t home_get_handler(httpd_req_t *req) {
+    httpd_resp_set_type(req, "text/html");
+
+    const uint32_t index_len = index_end - index_start;
+    httpd_resp_send(req, index_start, index_len);
+
+    return ESP_OK;
+}
+
+static esp_err_t chroma_get_handler(httpd_req_t *req) {
+    httpd_resp_set_type(req, "image/png");
+
+    const uint32_t chroma_len = chroma_end - chroma_start;
+    httpd_resp_send(req, chroma_start, chroma_len);
+
+    return ESP_OK;
+}    
+
 static const httpd_uri_t api = {
     .uri = "/api",
     .method = HTTP_GET,
     .handler = api_get_handler
+};
+
+static const httpd_uri_t home = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = home_get_handler
+};
+
+static const httpd_uri_t chroma = {
+    .uri = "/chroma.png",
+    .method = HTTP_GET,
+    .handler = chroma_get_handler
 };
 
 void web_server_init() {
@@ -79,6 +114,8 @@ void web_server_init() {
 
     if (httpd_start(&server, &config) == ESP_OK) {
         httpd_register_uri_handler(server, &api);
+        httpd_register_uri_handler(server, &home);
+        httpd_register_uri_handler(server, &chroma);
         return;
     }
     printf("Error al iniciar servidor\n");
